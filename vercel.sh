c2d85c53a0cd97fc6f1d9c919f39c7f57c763d23
Anders Evjen <evjand@gmail.com>
2025-10-01 12:46:27 +0200

    Add vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ "$VERCEL_TARGET_ENV" == 'preview' ]]; then
-  npx convex env set --preview-name $VERCEL_GIT_COMMIT_REF JWT_PRIVATE_KEY $JWT_PRIVATE_KEY
-  npx convex env set --preview-name $VERCEL_GIT_COMMIT_REF JWKS $JWKS
+  npx convex env set --preview-name $VERCEL_GIT_COMMIT_REF JWT_PRIVATE_KEY "$JWT_PRIVATE_KEY"
+  npx convex env set --preview-name $VERCEL_GIT_COMMIT_REF JWKS "$JWKS"
 fi
