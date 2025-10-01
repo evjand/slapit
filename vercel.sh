b80433eeb8a2ca7a674914521a85cd6e8578b6ed
Anders Evjen <evjand@gmail.com>
2025-10-01 12:54:49 +0200

    Escape private key

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ "$VERCEL_TARGET_ENV" == 'preview' ]]; then
-  npx convex env set --preview-name $VERCEL_GIT_COMMIT_REF JWT_PRIVATE_KEY "$JWT_PRIVATE_KEY"
-  npx convex env set --preview-name $VERCEL_GIT_COMMIT_REF JWKS "$JWKS"
+  JWT_PRIVATE_KEY_ESCAPED=$(printf '%s' "$JWT_PRIVATE_KEY")
+  bunx convex env set --preview-name $VERCEL_GIT_COMMIT_REF JWT_PRIVATE_KEY "$JWT_PRIVATE_KEY_ESCAPED"
+  bunx convex env set --preview-name $VERCEL_GIT_COMMIT_REF JWKS "$JWKS"
 fi
